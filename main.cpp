567f0d9ec2570afff2023646e8872ff0117dda28
cdevy <c.devy95@gmail.com>
2017-02-04 14:12:21 +0100

    Saisie puis affichage du mot mystère

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string mot;
+  cout << "Saisissez un mot : " << endl;
+  cin >> mot;
+  cout << "Le mot mystère est : " << mot << endl;
+  return 0;
+}
